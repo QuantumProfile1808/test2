3a688d6d1a834d9176276314d2af70ae755b43d7
QuantumProfile1808 <matiasscaboni@gmail.com>
2025-05-31 11:41:59 -0300

    añadi seralizers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RootSerializer
+from .models import Room
 
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
